7202aa791974eeffa7f98c177934e29891f5bb30
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2025-08-20 12:36:05 +0600

    sdf

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // -----------------
 // Middleware
